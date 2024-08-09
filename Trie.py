9a238b46892ac1343d19ba662dfd172da5e16403
Gabriel Ricci Pazinatto <gabrielrpazinatto@gmail.com>
2024-08-08 21:53:43 -0300

    Adicionados comentários

--- Trie.py
+++ Trie.py
@@ -4,51 +4,63 @@ class _TrieNode:
     def __init__(self) -> None:
         self.children = [None for _ in range(26)]
         self.is_final = False
-        self.value = []
+        self.value = None
         
 class Trie:
     def __init__(self) -> None:
         self.root: _TrieNode = _TrieNode()
         
+    # Insere uma tupla (key : value) na árvore
     def insert(self, key:str, value:any):
+        # Gambiarra para remover todos os caracteres especiais e letras maiúsculas dos nomes
         key = unicodedata.normalize('NFKD', key).encode('ascii',errors='ignore').decode('ascii').lower()
         node = self.root
         
         for c in key:
             if c in [' ', '.', '-', ',', '\'']: continue
+            # Acessa as sub-arvores correspondentes a cada caractere
             index = ord(c) - ord('a')
-            if node.children[index] is None:
-                node.children[index] = _TrieNode()
+            if node.children[index] is None:         # Se não houver o nodo correspondente ao caractere, cria-o
+                node.children[index] = _TrieNode()  
             node = node.children[index]
             
-        node.is_final = True
+        node.is_final = True                         # Marca o Nodo como final e armazena as informações nele
         node.value = value
         
+    # Dada uma chave, retorna seu valor correspondente
     def search(self, key:str):
+        # Gambiarra para remover todos os caracteres especiais e letras maiúsculas dos nomes
         key = unicodedata.normalize('NFKD', key).encode('ascii',errors='ignore').decode('ascii').lower()
         node = self.root
         for c in key:
             if c in [' ', '.', '-', ',', '\'']: continue
+            # Acessa as sub-arvores correspondentes a cada caractere
             index = ord(c) - ord('a')
-            if node.children[index] is None:
-                return None
-            else: node = node.children[index]
+            if node.children[index] is None:   # Se o nodo correspondente ao caractere não existir,
+                return None                    # a informação não está na árvore, retorna None
+            else: node = node.children[index]  
             
         return node.value    
         
+    
     def update_key(self, key:str, new_value:any):
+        # Gambiarra para remover todos os caracteres especiais e letras maiúsculas dos nomes
         key = unicodedata.normalize('NFKD', key).encode('ascii',errors='ignore').decode('ascii').lower()
         node = self.root
         
         for c in key:
             if c in [' ', '.', '-', ',', '\'']: continue
             index = ord(c) - ord('a')
+            # Acessa as sub-arvores correspondentes a cada caractere
             if node.children[index] is None:
                 node.children[index] = _TrieNode()
             node = node.children[index]
             
-        node.is_final = True
+        node.is_final = True              
         try:
-            node.value.append(new_value)
-        except:
-            node.value = [new_value]
+            node.value.append(new_value)    # Se o valor for uma lista, adiciona à lista
+        except:                             # Se não, transforma em lista
+            old_value = node.value
+            node.value = [new_value]        
+            if old_value != None:
+                node.value.append(old_value)
--- data_handler.py
+++ data_handler.py
@@ -3,16 +3,29 @@ import pickle
 from Hash import Hash
 from Trie import Trie
 
-functions = Hash(
-    entries={
-    'Saúde' : 0,
-    'Educação' : 1,
-    'Urbanismo' : 2,
-    'Agricultura' : 3,
-    'Assistência social' : 4,
-    'Outros' : 5
-})
+##########################################################################
+#                           FUNCIONAMENTO DO PROGRAMA
+#
+# Dados coletados de "Emendas.csv"
+# 
+# Dados normalizados e armazenados em listas da seguinte maneira:
+# [uid, value, state, function, author, year]
+# 
+# Armazena-se cada lista sequencialmente no arquivo "Amendments.bin" (cada carregamento
+# retorna uma emenda).
+# 
+# Gera-se um arquivo invertido "Pointers.bin", o qual, para cada carregamento realizado,
+# retorna uma lista de ponteiros para emendas mapeadas por função, na mesma 
+# ordem encontrada na variável functions_record.
+# 
+# Quando cada emenda é processada, é armazenado o nome de seu autor em uma
+# árvore Trie. Ele é utilizado para indexar uma lista de ponteiros para todas as 
+# emendas atreladas a esse nome. Então, gera-se um terceiro arquivo "Authors.bin",
+# onde é armazanada a árvore.
+# 
+##########################################################################
 
+# Dicionário de mapeamento de funções para emendas
 functions_record = Hash(
     entries={
     'Saúde' : [],
@@ -23,22 +36,41 @@ functions_record = Hash(
     'Outros' : []
 })
 
+# Árvore de indexação de Autores para emendas
 authors_record = Trie()
 
-def generate_authors_file(file, pointers:Trie = authors_record):
-    pickle.dump(file=file, obj=pointers)
+##########################################################################
 
+# Atualiza a árvore de índices de autores para emendas
 def update_authors_record(item, main_file):
     authors_record.update_key(item['author'], main_file.tell())
 
+# Atualiza o registro de emendas por função
 def update_functions_record(item, main_file):
-    if item['function'] not in functions.keys():
+    # Se a função não for uma das escolhidas, é armazenada como "Outros"
+    if item['function'] not in functions_record.keys():
         functions_record['Outros'].append(main_file.tell())
     else:
         functions_record[item['function']].append(main_file.tell())
+
+##########################################################################
+
+# Armazena a árvore Trie em um arquivo binário
+def generate_authors_file(file, pointers:Trie = authors_record):
+    pickle.dump(file=file, obj=pointers)
+
+# Armazena as listas de ponteiros para emendas por função
+def generate_pointers_file(file, pointers = functions_record):
+    for key in ['Saúde', 'Educação', 'Urbanismo', 'Agricultura', 'Assistência social', 'Outros']:
+        item = pointers[key]
+        pickle.dump(file = file, obj=item)
             
+# Gera o arquivo principal e armazena informações sobre as emendas
+# para geração dos demais arquivos.
 def generate_main_file(chunk, rows_read:int, main_file):
     chunk = chunk.to_dict(orient = 'records')
+    
+    # Gera um id unico para cada emenda
     uids = [i for i in range(rows_read, rows_read + len(chunk))]
 
     for i in range(len(chunk)):
@@ -49,38 +81,45 @@ def generate_main_file(chunk, rows_read:int, main_file):
         
         # Gera listas de ponteiro para itens de cada função
         update_functions_record(item=item, main_file= main_file)
+        # Atualiza a árvore Trie com os nomes presentes no csv e suas respectivas emendas
         update_authors_record(item = item, main_file = main_file)
         
+        # Transforma os itens do dicionário em uma lista, tornando o armazenamento mais eficiente
         item = [item['uid'], item['value'], item['state'], item['function'], item['author'], item['year']]
          
-        # Insere entrada no arquivo de armazenamento principal
+        # Insere entrada (emenda) no arquivo de armazenamento principal
         pickle.dump(file= main_file, obj= item)
-                    
-def generate_pointers_file(file, pointers = functions_record):
-    for key in ['Saúde', 'Educação', 'Urbanismo', 'Agricultura', 'Assistência social', 'Outros']:
-        item = pointers[key]
-        pickle.dump(file = file, obj=item)
+
 
 def generate_bin_files(input_file_path: str, chunk_size:int = 100000, offsets:list = []):
+    
+    # Abre os arquivos a serem escritos e passa por referência para as funções
+    # Evita que sejam abertos múltiplas vezes
     main_file = open("Amendments.bin", "wb+")
     pointers_file = open("Pointers.bin", "wb+")
     authors_file = open("Authors.bin", "wb+")
     
+    # Lê o arquivo de 100.000 em 100.000 linhas
     rows_read = 0
     for chunk in pd.read_csv(
         input_file_path, encoding='ISO-8859-15',
         on_bad_lines='warn', header=0, sep=';',
         low_memory=False, chunksize=chunk_size):
         
+        # Arquivo que armazena todas as entradas
         generate_main_file(chunk, rows_read, main_file)
     
+    # Arquivos invertidos
     generate_pointers_file(file = pointers_file, pointers = functions_record)
     generate_authors_file(file = authors_file, pointers = authors_record)
         
+    authors_file.close()
     main_file.close()
     pointers_file.close()
     
+# Processa uma entrada do arquivo csv
 def process_entry(row:pd.DataFrame, uid:int)->list:
+
     item = Hash(
         entries={
         'uid' : -1,
@@ -90,6 +129,8 @@ def process_entry(row:pd.DataFrame, uid:int)->list:
         'author' : '',
         'year' : -1        
     }, size = 6)
+
+    # Transforma a entrada em um dicionário
 
     value = 0
     value += float(row["Valor Empenhado"].replace(',', '.'))
@@ -108,6 +149,9 @@ def process_entry(row:pd.DataFrame, uid:int)->list:
     
     return item
 
+
+# Dado um campo "localidade" de uma emenda, retorna o estado mencionado na string
+# NORMALIZAR!!!!!!!!!
 def _get_state_name(s:str):
     if 'UF' in s:
         for i in range(len(s)):
