4d8246720e4be53615c28c8598048d94995bd6c3
Gabriel Ricci Pazinatto <gabrielrpazinatto@gmail.com>
2024-08-20 18:12:29 -0300

    Menu bonitinho

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from Search import Searcher
 import time
 import Update 
+import os
 
 if __name__ == '__main__':
     
-    print("Inicializando...")
+    print("\nInicializando...")
     start = time.process_time()
 
     data_manager = Searcher()
     try:data_manager.load_data()
     except: print("Dados não gerados!")
+    
+    order = False
     
     print("Programa inicializado em ",time.process_time() - start, 's!')
  
@@ -17,27 +20,22 @@ if __name__ == '__main__':
     
     while True:
         print("(1) Atualizar base de dados.")
+        print("\nBUSCAS POR CHAVE------------------------")
         print("(2) Buscar emendas por nome do autor.")
         print("(3) Buscar emendas por função.")
         print("(4) Buscar emendas por localidade. ")
+        print("------------------------------------------")
+        print("\nDADOS PROCESSADOS-----------------------")
         print("(5) Mostrar emendas por ano. ")
         print("(6) Mostrar emendas por função. ")
         print("(7) Mostrar emendas por localidade. ")
-        print("(0) Encerrar.")
+        print("------------------------------------------")
+        print("\n(8) Mudar ordenação dos dados. " + ("(CRESCENTE)" if order else "(DECRESCENTE)"))
+        print("(0) Encerrar.\n")
         
-        choice = input()
+        choice = input("Opção: ")
 
-        
-        print("Escolha a ordem dos valores: ")
-        print("(1) CRESCENTE. ")
-        print("(2) DESCRESCENTE. ")
-        order = input()
-
-        match(choice):
-            case '1':
-                order = True
-            case '2':
-                order = False
+        os.system('cls' if os.name == 'nt' else 'clear')
 
         match(choice):
             case '1':
@@ -54,6 +52,8 @@ if __name__ == '__main__':
                 data_manager.show_total_by_function()
             case '7':
                 data_manager.show_total_by_locality()
+            case '8':
+                order = not order
             case '0':
                 exit()
             case _:
