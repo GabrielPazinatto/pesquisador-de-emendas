06d6e89fcac52d59a137a81e30a2f9241d6b6936
Gabriel Ricci Pazinatto <gabrielrpazinatto@gmail.com>
2024-08-21 01:25:37 -0300

    Menu atualizado

--- main.py
+++ main.py
@@ -20,17 +20,18 @@ if __name__ == '__main__':
     
     while True:
         print("(1) Atualizar base de dados.")
+        print("(2) Reinicializar programa.")
         print("\nBUSCAS POR CHAVE------------------------")
-        print("(2) Buscar emendas por nome do autor.")
-        print("(3) Buscar emendas por função.")
-        print("(4) Buscar emendas por localidade. ")
+        print("(3) Buscar emendas por nome do autor.")
+        print("(4) Buscar emendas por função.")
+        print("(5) Buscar emendas por localidade. ")
         print("------------------------------------------")
         print("\nDADOS PROCESSADOS-----------------------")
-        print("(5) Mostrar emendas por ano. ")
-        print("(6) Mostrar emendas por função. ")
-        print("(7) Mostrar emendas por localidade. ")
+        print("(6) Mostrar emendas por ano. ")
+        print("(7) Mostrar emendas por função. ")
+        print("(8) Mostrar emendas por localidade. ")
         print("------------------------------------------")
-        print("\n(8) Mudar ordenação dos dados. " + ("(CRESCENTE)" if order else "(DECRESCENTE)"))
+        print("\n(9) Mudar ordenação dos dados. " + ("(CRESCENTE)" if order else "(DECRESCENTE)"))
         print("(0) Encerrar.\n")
         
         choice = input("Opção: ")
@@ -40,19 +41,22 @@ if __name__ == '__main__':
         match(choice):
             case '1':
                 data_manager.update_data_set()
-            case '2':
-                data_manager.search_by_author(order)
+                print("\nBase de dados atualizada. Reinicialize o programa.\n")
+            case 2:
+                data_manager.load_data()
             case '3':
-                data_manager.search_by_function(order)
+                data_manager.search_by_author(order)
             case '4':
-                data_manager.search_by_local(order)
+                data_manager.search_by_function(order)
             case '5':
-                data_manager.show_total_by_year()
+                data_manager.search_by_local(order)
             case '6':
-                data_manager.show_total_by_function()
+                data_manager.show_total_by_year()
             case '7':
-                data_manager.show_total_by_locality()
+                data_manager.show_total_by_function()
             case '8':
+                data_manager.show_total_by_locality()
+            case '9':
                 order = not order
             case '0':
                 exit()
